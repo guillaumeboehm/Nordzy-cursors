37415daefb2515561f63dd1c43728360e0deb700
Guillaume BOEHM <git@mail.gboehm.com>
2026-04-15 18:38:57 +0200

    fix: run CursorCreate from the python module dir

--- tools/generate_CursorCreate.sh
+++ tools/generate_CursorCreate.sh
@@ -4,17 +4,17 @@ script_dir="$(realpath "$(dirname "${0}")")"
 xcursors_dir="${script_dir}/../xcursors"
 cc_dir="${script_dir}/CursorCreate"
 cc_build_file="${script_dir}/CC_build.json"
-cc_script="${cc_dir}/CursorCreate/cursorcreate.py"
+cc_script="CursorCreate/cursorcreate.py" # Local to cc_dir
 
 themes_to_archive="Nordzy-cursors Nordzy-cursors-white Nordzy-cursors-lefthand Nordzy-cursors-white-lefthand"
 
 # Setup python env
-pushd "${script_dir}" > /dev/null || exit 1
+pushd "${script_dir}" >/dev/null || exit 1
 git submodule update --init -f
-python -m venv CC_venv > /dev/null
-source CC_venv/bin/activate > /dev/null
-pip install -r "${script_dir}/CursorCreate/requirements.txt" > /dev/null
-popd > /dev/null || exit 1
+python -m venv CC_venv >/dev/null
+source CC_venv/bin/activate >/dev/null
+pip install -r "${script_dir}/CursorCreate/requirements.txt" >/dev/null
+popd >/dev/null || exit 1
 
 tmp_themes_dir="${cc_dir}/themes"
 
@@ -35,12 +35,14 @@ for theme in ${themes}; do
         ln -sf "$file" "${theme_dir}/$(basename "$file")"
     done
 
+    pushd "${cc_dir}" >/dev/null || exit 1
     # Build the cursors
     python "${cc_script}" --build "${theme_dir}/$(basename "${cc_build_file}")"
+    popd >/dev/null || exit 1
 
     # Cp the windows cursors
     windows_zip="${theme_dir}/windows/${theme_name}.zip"
-    unzip -o "${windows_zip}" -d "${script_dir}/../Windows_cursors" > /dev/null
+    unzip -o "${windows_zip}" -d "${script_dir}/../Windows_cursors" >/dev/null
 
     # Cp the macos cursors
     macos_file="${theme_dir}/mousecape_macos/${theme_name}.cape"
